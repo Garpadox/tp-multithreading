83a27897a9697fcf96e5972e518df459e16b2a49
Garpadox <garpadox@gmail.com>
2026-01-19 11:51:36 +0100

    TP4 : Convertion des donneés

--- low_level/low_level.cpp
+++ low_level/low_level.cpp
@@ -1,6 +1,8 @@
 #include <cpr/cpr.h>
 #include <iostream>
+#include <nlohmann/json.hpp> // AJOUT : La librairie JSON
 #include <string>
+using json = nlohmann::json;
 
 class Task {
 private:
@@ -11,7 +13,6 @@ private:
 public:
   Task(std::string id, int s, float d) : identifier(id), size(s), duration(d) {}
 
-  // Méthode d'affichage
   void display() const {
     std::cout << "=== Details de la Tache ===" << std::endl;
     std::cout << "Identifier : " << identifier << std::endl;
@@ -22,16 +23,30 @@ public:
 };
 
 int main() {
-  std::cout << "--- Test de connexion HTTP ---" << std::endl;
+  std::cout << "Parsing JSON" << std::endl;
+
   std::string url = "http://127.0.0.1:8000/api/task/1/";
+  cpr::Response r = cpr::Get(cpr::Url{url}, cpr::Timeout{5000});
 
-  std::cout << "Requete vers : " << url << " ..." << std::endl;
-  cpr::Response r = cpr::Get(cpr::Url{url});
-
-  // 3. Vérification du statut HTTP
   if (r.status_code == 200) {
-    std::cout << "Succes ! Reponse recue :" << std::endl;
-    std::cout << r.text << std::endl;
+    std::cout << "Donnees brutes recues." << std::endl;
+
+    try {
+      json task_data = json::parse(r.text);
+      std::string id_recupere = task_data["identifier"];
+      int size_recuperee = task_data["size"];
+
+      float duration_recuperee = task_data.value("duration", 0.0f);
+
+      std::cout << "\n--- Analyse reussie via JSON ---" << std::endl;
+      std::cout << "Identifier: " << id_recupere << std::endl;
+      std::cout << "Size: " << size_recuperee << std::endl;
+      std::cout << "Duration: " << duration_recuperee << std::endl;
+
+    } catch (json::parse_error &e) {
+      std::cerr << "Erreur de parsing JSON : " << e.what() << std::endl;
+    }
+
   } else {
     std::cerr << "Erreur HTTP : " << r.status_code << std::endl;
     std::cerr << "Message : " << r.text << std::endl;
